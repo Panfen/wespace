0821ee2d23a917c3f67221bcd73868823f4bbeda
panfen <2809956575@qq.com>
2017-03-02 13:43:08 +0800

    'update'

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,7 +3,7 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '共享创造价值',
     userInfo: {}
   },
   //事件处理函数
--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -2,13 +2,20 @@
 var util = require('../../utils/util.js')
 Page({
   data: {
-    logs: []
+    default: "点我呀",
+    hello:"say hello",
+    news:['aaa','bbb','ccc']
   },
   onLoad: function () {
     this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(function (log) {
+      logs: (wx.getStorageSync('logs') || []).map(function (log)       {
         return util.formatTime(new Date(log))
       })
     })
+  },
+  defaultButton:function(){
+    this.setData({
+      hello:'toooooo'
+    });
   }
 })
