1f0a1e65f990643eb45955391f7c18df1bb03b05
panfen <2809956575@qq.com>
2017-03-04 22:11:21 +0800

    'update'

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,23 +4,58 @@ var app = getApp();
 Page({
   data: {
     motto: '共享创造价值',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../item/item?name=Panfen'
-    })
-    //或者直接在wxml页面使用<navigator url=""></navigator>控件
+    userInfo: {},
+    imgUrls: [
+      '../../images/swiper1.jpg',
+      '../../images/swiper2.jpg',
+      '../../images/swiper3.jpg',
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+    officeItems:[
+      {
+        pic:'../../images/pic1.jpg',
+        text:'精品办公室'
+      },
+      {
+        pic:'../../images/pic2.jpg',
+        text:'服务式办公室-新趋势'
+      },
+      {
+        pic:'../../images/pic3.jpg',
+        text:'办公室设计效果'
+      },
+      {
+        pic:'../../images/pic4.jpg',
+        text:'简洁办公室'
+      }
+    ],
+    meetingItems:[
+      {
+        pic:'../../images/pic5.jpg',
+        title:'黄龙体育中心高级会议场所',
+        rank:'5',
+        location:'黄龙体育中心',
+        price:'¥399/小时'
+      },
+      {
+        pic:'../../images/pic6.jpg',
+        title:'圣西罗大酒店会议厅',
+        rank:'4',
+        location:'留下镇屏峰社区',
+        price:'¥199/小时'
+      },
+
+    ],
   },
   onLoad: function () {
     var that = this
-    //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
-      //更新数据
       that.setData({
         userInfo:userInfo
       });
     });
-  }
+  },
 });
