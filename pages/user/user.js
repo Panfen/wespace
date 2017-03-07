d407ae34e3d6998aa7aaab80568aff74375d95a1
panfen <2809956575@qq.com>
2017-03-07 22:36:27 +0800

    'udpate'

--- pages/user/user.js
+++ pages/user/user.js
@@ -2,45 +2,35 @@
 var app = getApp();
 Page({
   data:{
-    username:'Panfen',
-    showConfirmBox:true
+    
   },
   onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
-    /*
-    if(app.appData.userInfo == null){
-      wx.redirectTo({url: '../login/login'});
-    }else{
-      this.setData({username:app.appData.userInfo.username});
-    }
-    */
   },
-  showBox:function(){
-    this.setData({
-      showConfirmBox:false
-    });
-  },
-  confirmBox:function(){
+
+  uploadFile:function(){
     var that = this;
-    wx.request({
-      url: 'https://ongdghewxx.localtunnel.me/upload',
+    wx.chooseImage({
       success: function(res) {
-        console.log(res.data.msg)
-      },
-      fail:function(res){
-        console.log('request fail!');
-      },
-      complete:function(){
-        that.setData({
-          showConfirmBox:true
+        var tempFilePaths = res.tempFilePaths
+        wx.uploadFile({
+          url: 'http://localhost:3000/upload', 
+          filePath: tempFilePaths[0],
+          name: 'file',
+          formData:{
+            'logo': 'myLogo'
+          },
+          success: function(res){
+            var data = JSON.parse(res.data)
+            console.log(data);
+          },
+          fail:function(){
+            console.log('upload fail');
+          },
+          complete:function(){
+            console.log('request finished');
+          }
         });
       }
     });
-  },
-  cancelBox:function(){
-    console.log(0)
-    this.setData({
-      showConfirmBox:true
-    });
-  },
+  }
 })
