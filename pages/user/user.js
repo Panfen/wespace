9acb1a4a2be1441e58ab16208bba612377135afc
panfen <2809956575@qq.com>
2017-03-08 22:02:18 +0800

    'udpate'

--- pages/user/user.js
+++ pages/user/user.js
@@ -2,35 +2,16 @@
 var app = getApp();
 Page({
   data:{
-    
+    bgPic:'../../images/bg.jpg',
+    userInfo:{}
   },
   onLoad:function(options){
-  },
-
-  uploadFile:function(){
     var that = this;
-    wx.chooseImage({
-      success: function(res) {
-        var tempFilePaths = res.tempFilePaths
-        wx.uploadFile({
-          url: 'http://localhost:3000/upload', 
-          filePath: tempFilePaths[0],
-          name: 'file',
-          formData:{
-            'logo': 'myLogo'
-          },
-          success: function(res){
-            var data = JSON.parse(res.data)
-            console.log(data);
-          },
-          fail:function(){
-            console.log('upload fail');
-          },
-          complete:function(){
-            console.log('request finished');
-          }
-        });
-      }
+    app.getUserInfo(function(info){
+      that.setData({userInfo:info});
     });
+  },
+  chooseHeader:function(){
+    console.log('hah')
   }
 })
