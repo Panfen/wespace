e040b7ad3f6b98628584d77bd9612f4d9691b102
panfen <2809956575@qq.com>
2017-03-19 22:12:29 +0800

    'update'

--- pages/myCollects/myCollects.js
+++ pages/myCollects/myCollects.js
@@ -1,8 +1,31 @@
 // pages/myCollects/myCollects.js
+var app = getApp();
 Page({
-  data:{},
+  data:{
+    collects:[
+      {
+        img:'../../images/pages/pic7.jpg',
+        title:'科院14号羽毛球场',
+        location:'杭州市西湖区留和路318号',
+        distance:'1.5km',
+        cost:'¥60/小时',
+        show:true
+      }
+    ],
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
+  viewSimilar:function(){
+    wx.navigateTo({url: '../allRents/allRents'});
+  },
+  removeCollect:function(e){
+    var index = e.target.dataset.index
+    var collects = this.data.collects;
+    collects[index].show = false;
+    this.setData({
+      collects:collects
+    });
+  }
 
 })
--- pages/myRemarks/myRemarks.js
+++ pages/myRemarks/myRemarks.js
@@ -3,6 +3,19 @@ Page({
   data:{
     animationData:{},
     menuOpen:false,
+    remarks:[
+      {
+        img:'../../images/pages/pic7.jpg',
+        title:'科院14号羽毛球场',
+        cost:'¥120',
+        time:'2017-03-12 21:28:43',
+        score1:4.5,
+        score2:4.5,
+        score3:4.0,
+        words:'这个体育馆是科院新建不久的，场地很新，打起来很舒服，价格60一小时还是有点贵。本校学生半价，外来人士可以找个学生一起打，这方法不错的。',
+        show:true,
+      }
+    ],
   },
   onLoad:function(options){
     //
@@ -23,5 +36,18 @@ Page({
       animationData:animation.export(),
       menuOpen:!this.data.menuOpen
     })
+  },
+
+  appendRemark:function(){
+    console.log('追加')
+  },
+
+  deleteRemark:function(e){
+    var index = e.target.dataset.index
+    var remarks = this.data.remarks;
+    remarks[index].show = false;
+    this.setData({
+      remarks:remarks
+    });
   }
 })
