1d77055c48398a730ab8d79002876a48940e1325
panfen <2809956575@qq.com>
2017-03-20 13:50:26 +0800

    'update'

--- pages/myRents/myRents.js
+++ pages/myRents/myRents.js
@@ -1,11 +1,26 @@
 // pages/myRents/myRents.js
 Page({
   data:{
-    showRemarkBox:false
+    showRemarkBox:false,
+    btnText:'评论',
+    words:''
   },
 
   writeRemark:function(){
     this.setData({showRemarkBox:true});
+  },
+
+  textareaInput:function(e){
+    var words = e.detail.value.trim();
+  },
+  submitRemark:function(){
+    this.setData({
+      btnText:'已评论',
+      showRemarkBox:false
+    });
+  },
+  cancelRemark:function(){
+    this.setData({showRemarkBox:false});
   }
 
 });
